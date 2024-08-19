84cd12936039641bbea4f04270926684316eff72
Damir Petrac <damir.petrac@if.se>
2024-08-20 01:06:40 +0200

    Merge fish config and bashrc

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,10 +6,14 @@ if status is-interactive
         # . (code --locate-shell-integration-path fish)
         set -gx SUDO_EDITOR "$(which code) -w"
         set -gx EDITOR "$(which code) -w"
+        set -gx VISUAL "$(which code) -w"
     end
 
     fzf_configure_bindings --directory=\cE --git_log=\eg --git_status=\eh --processes=\ep --variables=\ev
-    
+    abbr --add l ls -a
+    zoxide init fish | source
     # fnm env --use-on-cd | source
     # fish_ssh_agent
 end
+
+direnv hook fish | source
